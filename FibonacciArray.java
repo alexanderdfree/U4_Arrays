4b4f5b0e7be46a55a77fb0dbb619c053e9ffc158
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-02-02 11:19:19 -0800

    Update FibonacciArray.java
    first method completed

--- FibonacciArray.java
+++ FibonacciArray.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class FibonacciArray{
    public static int[] firstN(int n){
    /*Calculate the first n Fibonacci numbers
@@ -12,12 +13,16 @@ public class FibonacciArray{
    firstN(3) -> [0, 1, 1]
    firstN(5) -> [0, 1, 1, 2, 3]
    */
+   
    int[] sequenceTracker = new int[n];
+   StdOut.println(Arrays.toString(sequenceTracker));
    sequenceTracker[0] = 0;
+   if (n == 1) return sequenceTracker;
    sequenceTracker[1] = 1;
    for (int i = 2; i < n; i++) {
-      sequenceTracker[i] = sequenceTracker[i-1] + sequenceTracker[i+2];
+      sequenceTracker[i] = sequenceTracker[i-1] + sequenceTracker[i-2];
    }
+   StdOut.println(Arrays.toString(sequenceTracker));
    return sequenceTracker;
 
 

69df12cc3bfc31dfa24396b19dc6398fe420887d
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-02-02 11:13:18 -0800

    Create FibonacciArray.java
    fibonacci

---
+++ FibonacciArray.java
@@ -0,0 +1,25 @@
+public class FibonacciArray{
+   public static int[] firstN(int n){
+   /*Calculate the first n Fibonacci numbers
+   Input:
+      int n: how many fibonacci numbers to calculate
+   Output:
+      return: a int[] containing the first n Fibonacci
+              numbers.
+   Ex.
+   firstN(1) -> [0]
+   firstN(2) -> [0, 1]
+   firstN(3) -> [0, 1, 1]
+   firstN(5) -> [0, 1, 1, 2, 3]
+   */
+   int[] sequenceTracker = new int[n];
+   sequenceTracker[0] = 0;
+   sequenceTracker[1] = 1;
+   for (int i = 2; i < n; i++) {
+      sequenceTracker[i] = sequenceTracker[i-1] + sequenceTracker[i+2];
+   }
+   return sequenceTracker;
+
+
+}
+}

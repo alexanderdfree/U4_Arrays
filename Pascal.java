6445fd20df617c14653551fa4697f5a3e38cec97
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-02-02 11:38:30 -0800

    Create Pascal.java
    pascal made

---
+++ Pascal.java
@@ -0,0 +1,20 @@
+public class Pascal{
+   public static int[] nextRow(int[] prev){
+   /*Given a previous row, generate the next row
+   of Pascal's Triangle.
+   Input:
+      int[] prev: the previous row of Pascal's Triangle
+   Output:
+      return: a new int[] containing the next row of
+              Pascal's Triangle
+   Ex.
+           |||
+           vvv making an array directly
+   nextRow(new int[]{1}) -> [1, 1]
+   nextRow(new int[]{1, 1}) -> [1, 2, 1]
+   nextRow(new int[]{1, 2, 1}) -> [1, 3, 3, 1]
+   */
+   int[] newRow = new int[prev.length()+1]
+
+
+}
